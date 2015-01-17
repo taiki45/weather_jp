f58069f5f9aee48b0003b6d68325797beb9fb3b4
Taiki ONO <taiks.4559@gmail.com>
2015-01-17 22:36:24 +0900

    Update Weatherjp::City for new xml

--- lib/weather_jp/city.rb
+++ lib/weather_jp/city.rb
@@ -1,5 +1,30 @@
 module WeatherJp
-  class City < Struct.new(:name, :area_code)
+  class City
+    attr_reader :name
+
+    def initialize(name, attrs = {})
+      @name = name
+      @attrs = attrs
+    end
+
+    def full_name
+      @attrs['weatherlocationname']
+    end
+
+    def code
+      @attrs['weatherlocationcode']
+    end
+    alias_method :area_code, :code
+    alias_method :location_code, :code
+
+    def lat
+      @attrs['lat']
+    end
+
+    def long
+      @attrs['long']
+    end
+
     alias_method :to_s, :name
   end
 end
