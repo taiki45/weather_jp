febdb5ac01b068d69eaf665443055e0019c0aff4
Taiki ONO <taiks.4559@gmail.com>
2015-01-17 23:50:46 +0900

    Refactoring RequestParser

--- lib/weather_jp/request_parser.rb
+++ lib/weather_jp/request_parser.rb
@@ -4,12 +4,13 @@ module WeatherJp
     end
 
     class << self
+      dates_regexp = "今日|きょう|明日|あした|明後日|あさって|今|いま|3日後|３日後|4日後|４日後"
       RequestMatcher = /
-        ((?<city>.*)の
-        (?<day>今日|きょう|今|いま|明日|あした|明後日|あさって|３日後|４日後|3日後|4日後)
-        の(天気|てんき).*) |
-        ((?<day>今日|きょう|今|いま|明日|あした|明後日|あさって|３日後|４日後|3日後|4日後)の
-        (?<city>.*)の(天気|てんき))
+        (
+          (?<city>.*)の(?<day>#{dates_regexp})の(天気|てんき).*
+        )|(
+          (?<day>#{dates_regexp})の(?<city>.*)の(天気|てんき)
+        )
       /ux
 
       def parser(str)
@@ -18,20 +19,19 @@ module WeatherJp
           day = data[:day]
           case day
           when /今日|きょう/u
-            day = 'today'
+            day = :today
           when /明日|あした/u
-            day = 'tomorrow'
+            day = :tomorrow
           when /明後日|あさって/u
-            day = 'day_after_tomorrow'
-          when /3日後|３日後/u
+            day = :day_after_tomorrow
+          when /3日後|３日後/
             day = 3
-          when /4日後|４日後/u
+          when /4日後|４日後/
             day = 4
           when /今|いま/
-            day = 'current'
+            day = :current
           else
-            # TODO: error
-            raise WeatherJpError, "Can't parse given String"
+            raise 'oops...'
           end
 
           Request.new(data[:city], day)
